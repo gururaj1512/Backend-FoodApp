e6215560f8c85d6b817cea5a6825312f5fa8262c
gururaj1512 <gururajgurram1512@gmail.com>
2024-04-01 23:39:55 +0530

    Integrating Frontend

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json())
-app.listen(3000)
+app.listen(5000)
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 
